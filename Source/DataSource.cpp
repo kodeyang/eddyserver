08d2b1fb0b2cedc4165dcd73fe8ca884a6e92c50
zhangpanyi <zhangpanyi@live.com>
2015-08-10 19:45:42 +0800

    update

--- Source/DataSource.cpp
+++ Source/DataSource.cpp
@@ -1,5 +1,8 @@
 ﻿#include "DataSource.h"
 
+#include <QtGlobal>
+#include <QtAlgorithms>
+
 
 DataSource::DataSource()
 {
@@ -9,4 +12,70 @@ DataSource::DataSource()
 DataSource::~DataSource()
 {
 
+}
+
+void DataSource::addCategory(const QString &name)
+{
+	auto itr = all_records_.find(name);
+	if (itr != all_records_.end())
+	{
+		emit refresh(this);
+		emit message(CATEGORY_EXISTS, tr("category already exists, add category failed"));
+	}
+	else
+	{
+		category_order_.push_back(name);
+		all_records_.insert(name, std::move(QVector<PWData>()));
+		emit refresh(this);
+	}
+}
+
+void DataSource::deleteCategory(const QString &name)
+{
+	auto itr = all_records_.find(name);
+	if (itr != all_records_.end())
+	{
+		
+		all_records_.erase(itr);
+		emit refresh(this);
+	}
+	else
+	{
+		auto result = qFind(category_order_.begin(), category_order_.end(), name);
+		Q_ASSERT(result != category_order_.end());
+		category_order_.erase(result);
+
+		emit refresh(this);
+		emit message(CATEGORY_NOT_EXISTS, tr("category does not exist failed to delete the category"));
+	}
+}
+
+void DataSource::changeCategoryName(const QString &name, const QString &new_name)
+{
+	auto itr = all_records_.find(name);
+	if (itr != all_records_.end())
+	{
+		if (all_records_.find(new_name) == all_records_.end())
+		{
+			QVector<PWData> records(std::move(*itr));
+			all_records_.erase(itr);
+			all_records_.insert(new_name, std::move(records));
+
+			auto result = qFind(category_order_.begin(), category_order_.end(), name);
+			Q_ASSERT(result != category_order_.end());
+			*result = new_name;
+
+			emit refresh(this);
+		}
+		else
+		{
+			emit refresh(this);
+			emit message(CATEGORY_EXISTS, tr("existing category with the same name, rename category failed"));
+		}
+	}
+	else
+	{
+		emit refresh(this);
+		emit message(CATEGORY_NOT_EXISTS, tr("category does not exist, rename category failed"));
+	}
 }
--- Source/DataSource.h
+++ Source/DataSource.h
@@ -1,12 +1,43 @@
 ﻿#pragma once
 
+#include <QMap>
+#include <QVector>
 #include <QObject>
+
+struct PWData
+{
+	QString title;
+	QString user_name;
+	QString pass_word;
+	QString url;
+	QString notes;
+};
 
 class DataSource : public QObject
 {
 	Q_OBJECT
 
 public:
+	enum MessageCode
+	{
+		CATEGORY_EXISTS,
+		CATEGORY_NOT_EXISTS,
+	};
+
+public:
 	explicit DataSource();
 	~DataSource();
+
+public Q_SLOTS:
+	void addCategory(const QString &name);
+	void deleteCategory(const QString &name);
+	void changeCategoryName(const QString &name, const QString &new_name);
+
+Q_SIGNALS:
+	void refresh(DataSource *data_source);
+	void message(DataSource::MessageCode code, const QString &details);
+
+private:
+	QVector<QString> category_order_;
+	QMap<QString, QVector<PWData>> all_records_;
 };
