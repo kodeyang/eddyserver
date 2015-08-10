dc9a9009a70a8ba66f4a908435e72ed08612dcad
zhangpanyi <zhangpanyi@live.com>
2015-08-10 22:13:09 +0800

    update

--- Source/DataSource.cpp
+++ Source/DataSource.cpp
@@ -14,6 +14,52 @@ DataSource::~DataSource()
 
 }
 
+{
+	rapidjson::Document doc;
+	doc.Parse<0>(bytes.data());
+
+	if (doc.HasParseError())
+	{
+		qDebug() << "has parse error.";
+		return;
+	}
+
+	for (auto itr = doc.MemberBegin(); itr != doc.MemberEnd(); ++itr)
+	{
+		if (itr->name.IsString() && itr->value.IsArray())
+		{
+			const rapidjson::Value &json_array = itr->value;
+			for (size_t idx = 0; idx < json_array.Size(); ++idx)
+			{
+				if (json_array[idx].IsObject())
+				{
+					const rapidjson::Value &json_object = json_array[idx];
+					if (json_object.HasMember("title")
+						&& json_object.HasMember("user_name")
+						&& json_object.HasMember("pass_word")
+						&& json_object.HasMember("url")
+						&& json_object.HasMember("notes")
+						&& json_object["title"].IsString()
+						&& json_object["user_name"].IsString()
+						&& json_object["pass_word"].IsString()
+						&& json_object["url"].IsString()
+						&& json_object["notes"].IsString()
+						)
+					{
+						PWData data;
+						data.title = json_object["title"].GetString();
+						data.user_name = json_object["user_name"].GetString();
+						data.pass_word = json_object["pass_word"].GetString();
+						data.url = json_object["url"].GetString();
+						data.notes = json_object["notes"].GetString();
+						all_record_[itr->name.GetString()].push_back(std::move(data));
+					}
+				}
+			}
+		}
+	}
+}
+
 void DataSource::addCategory(const QString &name)
 {
 	auto itr = all_records_.find(name);
