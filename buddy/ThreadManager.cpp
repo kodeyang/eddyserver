ba4cb63872993a6f6bb1002da91a07b459c58b54
zhangpanyi@live.com <zhangpanyi@live.com>
2015-09-01 16:53:41 +0800

    update

---
+++ buddy/ThreadManager.cpp
@@ -0,0 +1,14 @@
+﻿#include "ThreadManager.h"
+
+#include <boost/assert.hpp>
+
+
+ThreadManager::ThreadManager(size_t thread_num)
+{
+	BOOST_ASSERT(thread_num > 0);
+}
+
+ThreadManager::~ThreadManager()
+{
+
+}
---
+++ buddy/ThreadManager.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include <boost/noncopyable.hpp>
+
+class ThreadManager final : public boost::noncopyable
+{
+public:
+	explicit ThreadManager(size_t thread_num = 1);
+	~ThreadManager();
+};
